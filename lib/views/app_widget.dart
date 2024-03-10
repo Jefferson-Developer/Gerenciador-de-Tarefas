97426769f610ac18a622206c2b73ec83b8a58945
Jefferson Oliveira <jpo.developer@gmail.com>
2024-03-10 16:11:59 -0300

    Criação os escopos dos modulos da home e login

--- lib/views/app_widget.dart
+++ lib/views/app_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:asuka/asuka.dart';
-import 'package:job_timer/controllers/core/ui/app_config_ui.dart';
+import 'package:job_timer/controllers/cores/ui/app_config_ui.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
