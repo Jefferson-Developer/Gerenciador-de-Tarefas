0434f1050d40fc6fb09de9fa9e3c96cf72148749
Jefferson <jpo.developer@gmail.com>
2024-09-15 11:23:36 -0300

    Refatoração do Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/controllers/helpers/firebase_initializer.dart';
 import 'package:job_timer/controllers/modules/app_module.dart';
-import 'package:job_timer/firebase_options.dart';
 import 'package:job_timer/views/app_widget.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await FirebaseInitializer.initialize();
   runApp(
     ModularApp(
       debugMode: false,
