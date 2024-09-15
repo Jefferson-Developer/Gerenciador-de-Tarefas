2880f71fe2bf5aef8bafd0ddacead6342517e0a8
Jefferson <jpo.developer@gmail.com>
2024-09-15 11:23:18 -0300

    Adicionado AuthService ao LoginModule

--- lib/controllers/modules/user_modules/login_module.dart
+++ lib/controllers/modules/user_modules/login_module.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:job_timer/controllers/services/firebase/auth/auth_service.dart';
 import 'package:job_timer/views/pages/login/login_page.dart';
 import 'package:job_timer/views/pages/login/login_state.dart';
 
@@ -6,6 +7,9 @@ class LoginModule extends Module {
   @override
   void binds(Injector i) {
     super.binds(i);
+    i.addLazySingleton<AuthService>(
+      () => AuthService(),
+    );
     i.addLazySingleton<LoginState>(
       () => LoginState(
         service: i.get(),
